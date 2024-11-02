957345b0904ad3cafe93c440afc11ec1b76983ef
MananG_Dev <mgarg0506@gmail.com>
2024-11-03 00:57:20 +0530

    User api functionality completed

--- src/main/java/com/blogapp/controllers/UserController.java
+++ src/main/java/com/blogapp/controllers/UserController.java
@@ -1,11 +1,14 @@
 package com.blogapp.controllers;
 
+import com.blogapp.payloads.ApiResponse;
 import com.blogapp.payloads.UserDto;
 import com.blogapp.services.impl.UserServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/users")
@@ -20,5 +23,31 @@ public class UserController {
         return new ResponseEntity<UserDto>(userDto1, HttpStatus.CREATED);
     }
 
-    //
+    // PUT - update User
+    @PutMapping("/{userID}")
+    public ResponseEntity<UserDto> updateUser(@RequestBody UserDto userDto, @PathVariable(name = "userID") Integer userId)  {
+        UserDto userDto1 = userService.updateUser(userDto, userId);
+        return new ResponseEntity<>(userDto1, HttpStatus.OK);
+    }
+
+    // GET - get User
+    @GetMapping("/{userID}")
+    public ResponseEntity<UserDto> getUser(@PathVariable(name="userID") Integer uid)    {
+        UserDto userDto = userService.getUser(uid);
+        return new ResponseEntity<>(userDto, HttpStatus.FOUND);
+    }
+
+    // GET - get List of Users
+    @GetMapping("/")
+    public ResponseEntity<List<UserDto>> getAllUsers()    {
+        List<UserDto> userDtos = userService.getAllUsers();
+        return new ResponseEntity<>(userDtos, HttpStatus.OK);
+    }
+
+    // DELETE - delete user
+    @DeleteMapping("/{userID}")
+    public ResponseEntity<?> deleteUser(@PathVariable(name="userID") Integer uid)   {
+        userService.deleteUser(uid);
+        return new ResponseEntity<>("Deleted", HttpStatus.OK);
+    }
 }
--- src/main/java/com/blogapp/exceptions/ResourceNotFoundException.java
+++ src/main/java/com/blogapp/exceptions/ResourceNotFoundException.java
@@ -7,7 +7,7 @@ public class ResourceNotFoundException extends RuntimeException {
 
     public ResourceNotFoundException(String source, String field, Object data)  {
 //        super(source + " not found with " + field + " having value " + data);
-        super(String.format("%s not found with %s: " + data.toString()));
+        super(source + " not found with " + field + " : " + data.toString());
         this.resourceName = source;
         this.fieldName = field;
         this.fieldValue = data;
